945216cb37d1b4bc4d09942306b19bfe117ab5ca
widiamka <43339745+widiamka@users.noreply.github.com>
2018-09-23 10:14:29 +0700

    Add files via upload

---
+++ PBO3_10117082_Latihan3_input/src/pbo3_10117082_latihan3_input/PBO3_10117082_Latihan3_input.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3_10117082_latihan3_input;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Nama : WIDIAMEGA K
+ *         Kelas : IF3
+ *         NIM    : 10117082
+ */
+public class PBO3_10117082_Latihan3_input {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("Masukkan nama anda: ");
+        Scanner scanner;
+        scanner = new Scanner (System.in);
+        
+        String nama = scanner.next();
+        System.out.println("Nama anda adalah " +nama);
+        
+        
+    
+    }
+    
+}
